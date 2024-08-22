00ef882617205b9a0cb63e9d737e9572aa15ba58
Omega0033 <omega@ramonpi.com>
2024-08-22 20:11:30 +0200

    Cambie letras

--- src/main/java/com/github/lapamanmodders/betterchest/block/ModBlocks.java
+++ src/main/java/com/github/lapamanmodders/betterchest/block/ModBlocks.java
@@ -23,7 +23,7 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> REMOTE_BLOCK = registerBlock("remote_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTab.REMOTE_TAB);
+                    .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.REMOTE_TAB);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
